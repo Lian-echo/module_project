568dfa294104219c45d6975c09f2d9cb85264458
qingyao.lian <qingyao.lian@upai.com>
2023-08-11 16:35:57 +0800

    feat:添加r-scale-screen配合大屏缩放、添加redirect

--- src/layouts/components/header.tsx
+++ src/layouts/components/header.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./style.less";
+import { history } from "umi";
+
 const header: React.FC<any> = () => {
   return (
     <div className={styles.headers} style={{ padding: "0 20px" }}>
-      <div></div>
+      <div
+        className={styles.headerL}
+        onClick={() => {
+          // history.push("/largeScreen");
+          window.open("/largeScreen");
+        }}
+      >
+        大屏
+      </div>
       <div className={styles.headerR}>
-        <img alt="headerImg" src={require("@/assets/c.png")}></img>
+        <img alt="headerImg" src={require("@/assets/c.png")} />
       </div>
     </div>
   );
