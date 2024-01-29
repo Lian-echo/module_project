3273a9f9807d415cde471609e33e533dac2ac6ba
qingyao.lian <qingyao.lian@upai.com>
2024-01-29 14:43:45 +0800

    feat:添加用户管理

--- src/pages/userManage/index.tsx
+++ src/pages/userManage/index.tsx
@@ -1,7 +1,8 @@
-import React from "react"
+import React from 'react';
 
+// 用户管理
 const UserManage = () => {
-  return <div>用户管理</div>
-}
+  return <div>用户管理</div>;
+};
 
-export default UserManage
+export default UserManage;
